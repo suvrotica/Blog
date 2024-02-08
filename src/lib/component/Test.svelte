184372fbe87798e4ebb1b864b1f80fe06c7debe1
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-02-08 21:24:35 +0530

    Add @tailwindcss/typography package and update styles

---
+++ src/lib/component/Test.svelte
@@ -0,0 +1,11 @@
+<h1>Hello</h1>
+<p class="paragraph">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias iure quasi vero aut blanditiis.
+	Laboriosam quam quasi aliquam dolorum deleniti nesciunt sapiente eum ex, nemo reiciendis quia
+	facilis tempora? Tenetur, quasi eveniet. Quae laudantium sapiente facilis! Similique incidunt
+	dolorem, adipisci maxime nulla veniam id alias natus facere neque corporis? Incidunt, voluptatem
+	ipsa saepe repudiandae tempore quos provident. Illum fugit error adipisci suscipit sed fuga culpa
+	fugiat exercitationem dolorem ab. Sed, amet voluptates. Dolorem qui perspiciatis vero
+	necessitatibus impedit consequatur alias! Porro facilis tenetur, illum ducimus harum earum fugit
+	quos eligendi soluta quo expedita, necessitatibus labore velit voluptas sapiente maxime? Autem.
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Hero from '$lib/component/Hero.svelte';
+	import Test from '$lib/component/Test.svelte';
 </script>
 
+<Test />
 <Hero />
