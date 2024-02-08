153901c227f4e1f9e6e64941fd158e47749dcf40
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-02-08 21:30:04 +0530

    Update styles and content in app.pcss and Test.svelte

--- src/lib/component/Test.svelte
+++ src/lib/component/Test.svelte
@@ -1,11 +1,13 @@
 <h1>Hello</h1>
 <p class="paragraph">
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias iure quasi vero aut blanditiis.
-	Laboriosam quam quasi aliquam dolorum deleniti nesciunt sapiente eum ex, nemo reiciendis quia
-	facilis tempora? Tenetur, quasi eveniet. Quae laudantium sapiente facilis! Similique incidunt
-	dolorem, adipisci maxime nulla veniam id alias natus facere neque corporis? Incidunt, voluptatem
-	ipsa saepe repudiandae tempore quos provident. Illum fugit error adipisci suscipit sed fuga culpa
-	fugiat exercitationem dolorem ab. Sed, amet voluptates. Dolorem qui perspiciatis vero
-	necessitatibus impedit consequatur alias! Porro facilis tenetur, illum ducimus harum earum fugit
-	quos eligendi soluta quo expedita, necessitatibus labore velit voluptas sapiente maxime? Autem.
+	Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of my
+	embarrassing tangle of nerves, unrelated fancies and figments strewn around and rattling loose in
+	it, collectibles from the waste basket scraps that should have seen a shredder, or, somewhat more
+	crudely, everything about anything. I'm not a prisoner to the usual boundaries that limit most
+	people, and I wanted a place where the diarist in me could jot down the wanderings of his
+	profligately traveling mind and not feel unwelcome. I presume I would leave the world richer in
+	nonsense than anything of substance. But here I explore, when I still can, the vast and forbidden
+	realms of human feats and foibles, fucks and farts, feces and fraternities. So whether you're a
+	seasoned simulationist or a curious commoner, an android or an amateur, a ponderer or a
+	pragmatist, join me in the infinite mosaic of collective nonsense we can explore.
 </p>
