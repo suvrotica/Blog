eaa0218071820dea5e59a781ad5844215f5aecf4
suvrotica <117360356+suvrotica@users.noreply.github.com>
2024-02-19 07:42:58 +0530

    Update Test.svelte

--- src/lib/component/Test.svelte
+++ src/lib/component/Test.svelte
@@ -9,10 +9,10 @@ const envelopeStyle="md:flex";
     
       <img src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20240112-WA0001-x3fHkx8kppU2AWVrincv0q8Km6reBL.jpg" alt="Suvro Ghosh">
     
-    
+    <div class="col-span-2">
       <h1 class="uppercase text-sm font-semibold">About this blog</h1>
       <h2 class="block mt-1 text-lg leading-tight font-medium hover:underline">The Ghosh Who Shits</h2>
-      <p class="col-span-2">Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of my
+      <p>Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of my
 	embarrassing tangle of nerves, unrelated fancies and figments strewn around and rattling loose in
 	it, collectibles from the waste basket scraps that should have seen a shredder, or, somewhat more
 	crudely, everything about anything. I'm not a prisoner to the usual boundaries that limit most
@@ -21,7 +21,8 @@ const envelopeStyle="md:flex";
 	nonsense than anything of substance. But here I explore, when I still can, the vast and forbidden
 	realms of human feats and foibles, fucks and farts, feces and fraternities. So whether you're a
 	seasoned simulationist or a curious commoner, an android or an amateur, a ponderer or a
-	pragmatist, join me in the infinite mosaic of collective nonsense we can explore.</p>
-    
+	pragmatist, join me in the infinite mosaic of collective nonsense we can explore.
+      </p>
+    </div>
   </div>
 </section>
