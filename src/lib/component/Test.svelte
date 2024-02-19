4f7a26a7246189d32285d750fbd796c349efbbcc
suvrotica <117360356+suvrotica@users.noreply.github.com>
2024-02-19 07:40:16 +0530

    Update Test.svelte

--- src/lib/component/Test.svelte
+++ src/lib/component/Test.svelte
@@ -5,14 +5,14 @@ const envelopeStyle="md:flex";
 
 </script>
 <section>
-  <div class="grid grid-cols-2 gap-4">
+  <div class="grid grid-cols-3 gap-4">
     
       <img src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20240112-WA0001-x3fHkx8kppU2AWVrincv0q8Km6reBL.jpg" alt="Suvro Ghosh">
     
     
       <h1 class="uppercase text-sm font-semibold">About this blog</h1>
       <h2 class="block mt-1 text-lg leading-tight font-medium hover:underline">The Ghosh Who Shits</h2>
-      <p class="mt-2">Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of my
+      <p class="col-span-2">Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of my
 	embarrassing tangle of nerves, unrelated fancies and figments strewn around and rattling loose in
 	it, collectibles from the waste basket scraps that should have seen a shredder, or, somewhat more
 	crudely, everything about anything. I'm not a prisoner to the usual boundaries that limit most
